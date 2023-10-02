85ea2685db35b20cf7c0e46066435964a00e6114
Ragnar-Oock <u.drgnk@outlook.com>
2023-10-02 20:27:58 +0200

    feat(event): start implementation of the Event interface

--- src/Core/event/event.model.ts
+++ src/Core/event/event.model.ts
@@ -1,33 +1,109 @@
-import { EVENT_PHASE, EventInterface } from "../../API/event.interface";
+import { EventTargetInterface } from "../../API/event-target.interface";
+import { EVENT_PHASE, EventInterface, EventType, EventInit } from "../../API/event.interface";
+import { SequenceInterface } from "../../API/sequence.interface";
+import { warn } from "../helper/log.helper";
 
-export type EventInitialValues = {
-    cancelable: boolean;
-}
-
-export default class Event<Type extends string, Payload> implements EventInterface<Type, Payload> {
-    private static readonly initialvalues: EventInitialValues = {
-        cancelable: false,
-    }
-
-    public readonly type: Type;
-    public readonly payload: Payload;
-    public readonly cancelable: boolean = false;
-    public readonly timeStamp: DOMHighResTimeStamp = performance.now();
+export default class Event<
+    EventName extends EventType,
+    Target extends EventTargetInterface = EventTargetInterface,
+    CurrentTarget extends EventTargetInterface = EventTargetInterface
+    > implements EventInterface<EventName, Target, CurrentTarget> {
         
-    private _defaultPrevented: boolean = false;
+    private _type: EventName;
+    private _bubbles: boolean;
+    private _cancelable: boolean;
+    private _target: Target | null = null;
+    private _currentTarget: CurrentTarget | null = null;
     private _eventPhase: EVENT_PHASE = EVENT_PHASE.NONE;
-    private _immediatePropagationStoped: boolean = false;
+    private _isTrusted: boolean = false;
+    private readonly _timeStamp: DOMHighResTimeStamp = performance.now();
 
-    constructor(type: Type, payload: Payload, initValues?: Partial<EventInitialValues>) {
-        this.type = type;
-        this.payload = payload;
-        this.cancelable = initValues?.cancelable ?? Event.initialvalues.cancelable;
+    /**
+     * @internal
+     */
+    public stopPropagationFlag: boolean = false;
+    /**
+     * @internal
+     */
+    public stopImmediatePropagationFlag: boolean = false;
+    /**
+     * @internal
+     */
+    private _canceledFlag: boolean = false;
+    /**
+     * @internal
+     */
+    public inPassiveListenerFlag: boolean = false;
+    /**
+     * @internal
+     */
+    public composedFlag: boolean;
+    /**
+     * @internal
+     */
+    public initializedFlag: boolean = false;
+    /**
+     * @internal
+     */
+    public dispatchFlag: boolean = false;
+
+
+    constructor(type: EventName, initValues?: EventInit) {
+        this._type = type;
+        this._cancelable = initValues?.cancelable ?? false;
+        this._bubbles = initValues?.bubbles ?? false;
+        this.composedFlag = initValues?.composed ?? false;
     }
 
-    public get defaultPrevented(): boolean {
-        return this._defaultPrevented;
+    /**
+     * @inheritdoc
+     */
+    public get type(): EventName {
+        return this._type;
     }
 
+    /**
+     * @inheritdoc
+     */
+    public get bubbles(): boolean {
+        return this._bubbles;
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public get cancelable(): boolean {
+        return this._cancelable;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get target(): Target | null {
+        return this._target;
+    }
+
+    public composedPath(): SequenceInterface<EventTargetInterface> {
+        return Object.freeze([]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get currentTarget(): CurrentTarget | null {
+        return this._currentTarget;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get timeStamp(): DOMHighResTimeStamp {
+        return this._timeStamp;
+    }
+
+    /**
+     * @inheritdoc
+     */
     public get eventPhase(): EVENT_PHASE {
         return this._eventPhase;
     }
@@ -35,20 +111,121 @@ export default class Event<Type extends string, Payload> implements EventInterfa
     /**
      * @inheritdoc
      */
-    public get immediatePropagationStoped(): boolean {
-        return this._immediatePropagationStoped;
+    public stopPropagation(): void {
+        this.stopPropagationFlag = true;
+    }
+    /**
+     * @inheritdoc
+     */
+    public get cancelBubble(): boolean {
+        return this.stopPropagationFlag === true;
+    }
+    /**
+     * @inheritdoc
+     */
+    public set cancelBubble(value: boolean) {
+        if (value) {
+            this.stopPropagationFlag = true;
+        }
+    }
+    /**
+     * @inheritdoc
+     */
+    public stopImmediatePropagation(): void {
+        this.stopPropagationFlag = true;
+        this.stopImmediatePropagationFlag = true;
     }
 
-    public preventDefault(): void;
+    /**
+     * @internal
+     */
+    public get canceledFlag(): boolean {
+        return this._canceledFlag;
+    }
+    /**
+     * @inheritdoc
+     */
+    public set canceledFlag(_value: boolean) {
+        if (this.cancelable && !this.inPassiveListenerFlag) {
+            this._canceledFlag = true;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get returnValue(): boolean {
+        return !this.canceledFlag;
+    }
+    /**
+     * @inheritdoc
+     */
+    public set returnValue(value: boolean) {
+        if (!value) {
+            this.canceledFlag = true;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
     public preventDefault(): void {
-        if (!this.cancelable) {
+        this.canceledFlag = true;
+
+        if (!this.canceledFlag) {
+            warn(`The event ${this}'s default behavior cannot be prevented as it was initialized with cancelable=false`)
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get defaultPrevented(): boolean {
+        return this.canceledFlag;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get composed(): boolean {
+        return this.composedFlag;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public get isTrusted(): boolean {
+        return this._isTrusted;
+    }
+
+    private static initialize<EventName extends EventType>(event: Event<EventName>, type: EventName, bubbles: boolean, cancelable: boolean): Event<EventName> {
+        event.initializedFlag = true;
+
+        event.stopPropagationFlag = false;
+        event.stopImmediatePropagationFlag = false;
+        event.canceledFlag = false;
+
+        event._isTrusted = false;
+
+        event._target = null;
+
+        event._type = type;
+        event._bubbles = bubbles;
+        event._cancelable = cancelable;
+
+        return event;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public initEvent(type: EventName, bubbles: boolean, cancelable: boolean): void {
+        if (this.dispatchFlag) {
             return;
         }
 
-        this._defaultPrevented = true;
+        Event.initialize(this, type, bubbles, cancelable);
     }
+    
 
-    public stopImmediatePropagation(): void {
-        this._immediatePropagationStoped = true;
-    }
 }
